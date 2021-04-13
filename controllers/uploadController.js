efebb93428a4b221dc847386e2010538fa3a37f1
naieem-bd <naieemur@gmail.com>
2021-04-13 11:18:40 +0600

    bug fix

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -9,13 +9,13 @@ exports.uploadProfilePics = async (req, res, next) => {
             if(profile) {
                 await Profile.findOneAndUpdate(
                     { user: req.user._id },
-                    { set: { profilePics } }
+                    { $set: { profilePics } }
                 )
             }
 
             await User.findOneAndUpdate(
                 { _id: req.user._id },
-                { set: {profilePics} }
+                { $set: { profilePics } }
             )
             res.status(200).json({
                 profilePics
