93fd46acc03c39ce6ea68405c40c8260ae472919
naieem-bd <naieemur@gmail.com>
2021-04-19 13:08:48 +0600

    error handle in post

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,19 +7,32 @@ exports.createPostGetController = (req, res, next) => {
     res.render('pages/dashboard/post/createPost', {
         title: 'Create a new post',
         error: {},
-        flashMessage: Flash.getMessage(req)
+        flashMessage: Flash.getMessage(req),
+        value: {}
     })
 }
 
 exports.createPostPostController = (req, res, next) => {
-
+    let { title, body, tags } = req.body
     let errors = validationResult(req).formatWith(errorFormatter)
 
-    console.log(errors.mapped())
+    if(!errors.isEmpty()) {
+        res.render('pages/dashboard/post/createPost', {
+            title: 'Create a new post',
+            error: errors.mapped(),
+            flashMessage: Flash.getMessage(req),
+            value: {
+                title,
+                body,
+                tags
+            }
+        })        
+    }
 
     res.render('pages/dashboard/post/createPost', {
         title: 'Create a new post',
         error: {},
-        flashMessage: Flash.getMessage(req)
+        flashMessage: Flash.getMessage(req),
+        value: {}
     })
 }
