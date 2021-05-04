caf1a67e8a05981000e93f324920adb5c3a9832e
naieem-bd <naieemur@gmail.com>
2021-05-04 15:45:51 +0600

    like controller added

---
+++ api/controllers/likeDislikeController.js
@@ -0,0 +1,50 @@
+const Post = require('../../models/Post')
+
+exports.likesGetController = async(req, res, next) => {
+    let { postId } = req.params
+    let userId = req.user._id
+    let liked = null
+
+    if(!req.user) {
+        return res.status(403).json({
+            error: 'you are not an authenticated user'
+        })
+    }
+
+    try{
+        let post = Post.findById(postId)
+        if(post.dislikes.includes(userId)) {
+            await Post.findOneAndUpdate(
+                { _id: postId },
+                { $pull: { 'dislikes': userId } }
+            )
+        }
+
+        if(post.likes.includes(userId)) {
+            await Post.findOneAndUpdate(
+                { _id: postId },
+                { $pull: { 'likes': userId } }
+            )
+            liked = false
+        } else {
+            await Post.findOneAndUpdate(
+                { _id: postId },
+                { $push: { 'likes': userId } }
+            )
+            liked = true
+        }
+
+        let updatedPost = await Post.findById(postId)
+        res.status(200).json({
+            liked,
+            totalLikes: updatedPost.likes.length,
+            totalDislikes: updatedPost.dislikes.length
+        })
+
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json({
+            error: 'Server error occurred'
+        })        
+    }
+}
