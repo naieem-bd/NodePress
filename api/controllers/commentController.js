ef4ef2d5ffd6156902aae88460e93be6dfd577eb
naieem-bd <naieemur@gmail.com>
2021-05-04 14:28:03 +0600

    dislike controller

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -41,5 +41,35 @@ exports.commentPostController = (req, res, next) => {
 }
 
 exports.replyCommentPostController = async (req, res, next) => {
+    let { commentId } = req.params
+    let { body } = req.body
 
+    if(!req.user) {
+        return res.status(403).json({
+            error: 'you are not an authenticated user'
+        })
+    }
+
+    let reply = {
+        body,
+        user: req.user._id
+    }
+
+    try {
+        await Comment.findOneAndUpdate(
+            { _id: commentId },
+            { $push: {'replies':reply} }
+        )
+
+        res.status(201).json({
+            ...reply,
+            profilePics: req.user.profilePics
+        })
+
+    } catch(e) {
+        console.log(e)
+        return res.status(500).json({
+            error: 'Server error occurred'
+        })        
+    }
 }
--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
+const { isAuthenticated } = require('../../middleware/authMiddleware')
 
-router.post('/comments/:postId', (req, res, next) => {
-    
-})
+const {
+    commentPostController,
+    replyCommentPostController
+} = require('../controllers/commentController')
 
-router.post('/comments/replies/:commentId', (req, res, next) => {
-    
-})
+router.post('/comments/:postId', isAuthenticated, commentPostController)
+router.post('/comments/replies/:commentId', isAuthenticated, replyCommentPostController)
+
+router.get('/likes/:postId', isAuthenticated, (req, res) => {})
+router.get('/dislikes/:postId', isAuthenticated, (req, res) => {})
 
 module.exports = router
