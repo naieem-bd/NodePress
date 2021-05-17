d1e2c017df39607ffbac136968b5647eb6985585
naieem-bd <naieemur@gmail.com>
2021-05-17 16:50:09 +0600

    explorer

--- controllers/exploreController.js
+++ controllers/exploreController.js
@@ -1,9 +1,18 @@
 const Flash = require('../utils/Flash')
+const Post = require('../models/Post')
 
-exports.explorerGetController = (req, res, next) => {
-    res.render('pages/explorer/explorer', {
-        title: 'Explore all posts',
-        filter: 'latest',
-        flashMessage: Flash.getMessage(req)
-    })
+exports.explorerGetController = async (req, res, next) => {
+
+    try {
+        let posts = await Post.find()
+        res.render('pages/explorer/explorer', {
+            title: 'Explore all posts',
+            filter: 'latest',
+            flashMessage: Flash.getMessage(req),
+            posts
+        })
+    } catch(e) {
+        next(e)
+    }
+
 }
--- middleware/setLocals.js
+++ middleware/setLocals.js
@@ -1,7 +1,21 @@
+const cheerio = require('cheerio')
+const moment = require('moment')
+
 module.exports = () => {
     return (req, res, next) => {
         res.locals.user = req.user
         res.locals.isLoggedIn = req.session.isLoggedIn
+        res.locals.truncate = html => {
+            let node = cheerio.load(html)
+            let text = node.text()
+
+            text = text.replace(/(\r\r|\n|\r)/gm, '')
+
+            if(text.length <= 100) return text
+
+            return text.substr(0, 100) + '...'
+        }
+        res.locals.moment = time => moment(time).fromNow()
 
         next()
     }
