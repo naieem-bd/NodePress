667dfa77b92c9d89a1b68fb4c7ece245bd62b91f
naieem-bd <naieemur@gmail.com>
2021-05-22 12:15:58 +0600

    reply comment

--- public/scripts/comment.js
+++ public/scripts/comment.js
@@ -27,6 +27,37 @@ window.onload = function() {
         }
     })
 
+    commentHolder.addEventListener('keypress', function(e) {
+        if(commentHolder.hasChildNodes(e.target)) {
+            if(e.key === 'Enter') {
+                // console.log('i am here')
+                let commentId = e.target.dataset.comment
+                let value = e.target.value
+
+                if(value) {
+                    let data = {
+                        body: value
+                    }
+                    let req = generateRequest(`/api/comments/replies/${commentId}`, 'POST', data)
+                    fetch(req)
+                        .then(res => res.json())
+                        .then(data => {
+                            let replyElement = createReplyElement(data)
+                            let parent = e.target.parentElement
+                            parent.previousElementSibling.appendChild(replyElement)
+                            e.target.value = ''
+                        })
+                        .catch(e => {
+                            console.log(e)
+                            alert(e.message)
+                        })
+                } else {
+                    alert('Please enter a reply')
+                }
+            }
+        }
+    })
+
 }
 
 function generateRequest(url, method, body) {
@@ -52,7 +83,6 @@ function createComment(comment) {
             style="width: 40px;">
         <div class="media-body my-3">
             <p>${comment.body}</p>
-            <p>${comment.user.username}</p>
 
             <div class="my-3">
                 <input type="text" class="form-control" 
@@ -65,6 +95,24 @@ function createComment(comment) {
 
     let div = document.createElement('div')
     div.className = 'media border'
+    div.innerHTML = innerHTML
+
+    return div
+}
+
+function createReplyElement(reply) {
+    let innerHTML = `
+        <img 
+            src="${reply.profilePics}" 
+            class="align-self-start rounded-circle mr-3"
+            style="width: 40px;">
+        <div class="media-body">
+            <p>${reply.body}</p>
+        </div>    
+    `
+
+    let div = document.createElement('div')
+    div.className = 'media mt-3'
     div.innerHTML = innerHTML
 
     return div
